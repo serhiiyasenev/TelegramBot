960326f3ab9a2a2873ff687f9378997dea629fef
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2022-01-24 20:36:11 +0200

    update start

--- TgBot/Services/Telegram/TelegramService.cs
+++ TgBot/Services/Telegram/TelegramService.cs
@@ -30,9 +30,9 @@ namespace TgBot.Services.Telegram
                     return;
                 }
 
-                if (update.Message.Text.Equals("/start"))
+                if (update.Message.Text.Equals("/start") || update.Message.Text.Equals("start"))
                 {
-                    await _telegramBotClient.SendTextMessageAsync(update.Message.Chat.Id, "Hi there!", cancellationToken: cancellationToken);
+                    await _telegramBotClient.SendTextMessageAsync(update.Message.Chat.Id, $"Hi there! {update.Message.Chat.FirstName}", cancellationToken: cancellationToken);
                     return;
                 }
 
